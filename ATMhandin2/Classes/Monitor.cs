83f8aafabc1be31671e4399e6828d9172609d0df
andreas <androgles1994@gmail.com>
2018-10-01 10:30:25 +0200

    test igen

--- ATMhandin2/Classes/Monitor.cs
+++ ATMhandin2/Classes/Monitor.cs
@@ -8,7 +8,20 @@ namespace ATMhandin2.Classes
 {
     public class Monitor
     {
-        //Dette er lige en test
+
+        public bool IsInsideAirSpace(List<SpecifikAircraft> Aircrafts)
+        {
+            foreach (var data in Aircrafts)
+            {
+                if (isInsideAirspace(data) == true)
+                {
+
+                }
+
+            }
+            return true;
+        }
+
         public bool isInsideAirspace(SpecifikAircraft aircraft)
         {
             if (aircraft.Altitude > 500 && aircraft.Altitude < 20000 && ((aircraft.XCoordinate > 10000 && aircraft.YCoordinate > 10000) || (aircraft.XCoordinate < 90000 && aircraft.YCoordinate < 90000)))
@@ -19,20 +32,9 @@ namespace ATMhandin2.Classes
 
             return false;
         }
-        //anne
+        //hajedajsdklsad
 
-        public bool IsInsideAirSpace(List<SpecifikAircraft> Aircrafts)
-        {
-            foreach (var data in Aircrafts)
-            {
-                if (isInsideAirspace(data) == true)
-                {
-
-                }
-               
-            }
-            return true;
-        }
+       
 
         //public bool seperation()
         //{
