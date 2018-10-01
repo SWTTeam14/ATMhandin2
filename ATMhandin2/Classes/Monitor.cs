f61f22111e70fbc383bb825d27da80dc99283f28
Anne Porsgaard <anne@besenbacher.com>
2018-10-01 10:30:22 +0200

    Slettede kommentarer

--- ATMhandin2/Classes/Monitor.cs
+++ ATMhandin2/Classes/Monitor.cs
@@ -8,7 +8,6 @@ namespace ATMhandin2.Classes
 {
     public class Monitor
     {
-        //Dette er lige en test
         public bool isInsideAirspace(SpecifikAircraft aircraft)
         {
             if (aircraft.Altitude > 500 && aircraft.Altitude < 20000 && ((aircraft.XCoordinate > 10000 && aircraft.YCoordinate > 10000) || (aircraft.XCoordinate < 90000 && aircraft.YCoordinate < 90000)))
@@ -19,7 +18,7 @@ namespace ATMhandin2.Classes
 
             return false;
         }
-        //anne
+        
 
         public bool IsInsideAirSpace(List<SpecifikAircraft> Aircrafts)
         {
