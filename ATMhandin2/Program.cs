485bd97ff0333a6b9bd21e704e842e9327d10951
RasmusChristensen <rasmus@christensen.io>
2018-10-02 10:17:54 +0200

    Added Program.cs back to the project folder /master branch

---
+++ ATMhandin2/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using ATMhandin2.Classes;
+using TransponderReceiver;
+
+namespace ATMhandin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITransponderReceiver receiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            var t1 = new Classes.TransponderReceiverClient(receiver);
+
+            AMSController amsController = new AMSController(t1);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                amsController.Print();
+            }
+        }
+    }
+}
