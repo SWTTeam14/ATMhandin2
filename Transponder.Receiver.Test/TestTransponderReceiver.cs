bafe04c69bb1047f75f2e22ceb9c373cb364efa6
andreas <androgles1994@gmail.com>
2018-10-02 15:28:08 +0200

    Added tests to AMS

--- Transponder.Receiver.Test/TestTransponderReceiver.cs
+++ Transponder.Receiver.Test/TestTransponderReceiver.cs
@@ -140,8 +140,6 @@ namespace Transponder.Receiver.Test
 
                 Assert.That(_uut.IsAircraftInside(td), Is.False);
             }
-
-
         }
 
         public class TestAircraft
@@ -157,8 +155,6 @@ namespace Transponder.Receiver.Test
 
                 _uut = new Airspace(10000, 10000, 90000, 90000, 500, 20000);
             }
-
-
         }
 
         public class TestAMSController
@@ -166,15 +162,41 @@ namespace Transponder.Receiver.Test
             private IAMSController _fakeAmsController;
             private TransponderReceiverClient trc;
             private AMSController _uut;
+            private ITransponderReceiver _fakeTransponderReceiver;
 
             [SetUp]
             public void Setup()
             {
                 _fakeAmsController = Substitute.For<IAMSController>();
-
+                _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+                trc = new TransponderReceiverClient(_fakeTransponderReceiver);
                 _uut = new AMSController(trc);
             }
 
+
+            [Test]
+            public void Add3planesToAirspaceAndRemoveOneBecauseOutOfAirSpace()
+            {
+                List<string> testData = new List<string>();
+                testData.Add("ATR423;39045;12932;14000;20151006213456789");
+                testData.Add("BCD123;10005;85890;12000;20151006213456789");
+                testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+                //ACT
+                _fakeTransponderReceiver.TransponderDataReady +=
+                    Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+                //Assert
+                
+                Assert.That(_uut._aircraftsInsideAirspace.Count, Is.EqualTo(3));
+                
+
+                testData[0] = "ATR423;91000;12932;14000;20151006213456789";
+                _fakeTransponderReceiver.TransponderDataReady +=
+                    Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+                Assert.That(_uut._aircraftsInsideAirspace.Count, Is.EqualTo(2));
+            }
 
         }
 
