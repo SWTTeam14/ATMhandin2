a46ba4ec5114956cfdafa9d2336af9426a4c813f
Anne Porsgaard <anne@besenbacher.com>
2018-10-03 15:26:48 +0200

    It was a mistake that we removed ITransponderDataItem, so we added it again.

---
+++ ATMhandin2/Interfaces/ITransponderDataItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ATMhandin2.Interfaces
+{
+    public interface ITransponderDataItem
+    {
+        int XCoordinate { get; set; }
+        int YCoordinate { get; set; }
+        int Altitude { get; set; }
+        DateTime TimeStamp { get; set; }
+        string ToString();
+    }
+}
